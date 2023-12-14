fe834a4053d88fed2d32cd4874b4ecdc179fdc02
Joshaw-k <awinrin4bless@yahoo.com>
2023-12-14 18:43:49 +0100

    first commit

---
+++ src/TshirtMerch.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract TshirtMerch{
+
+ address owner;
+
+ struct Merchandize {
+   string color;
+   uint amount;
+ }
+
+ Merchandize[] public merchandize;
+
+ uint public totalSupply = 40;
+
+ uint public totalSold;
+
+ modifier onlyOwner {
+    require(owner == msg.sender, "Only owner can call this function");
+    _;
+ }
+
+ error noZeroAmount();
+ error emptyColor();
+
+ constructor(){
+    owner = msg.sender;
+ }
+
+ function buyMerch(string calldata _color, uint _amount) public onlyOwner {
+   if(keccak256(abi.encode(_color)) == keccak256(abi.encode(""))){
+      revert emptyColor();
+   }
+   if(_amount <= 0){
+      revert noZeroAmount();
+   }
+   merchandize.push(Merchandize({color:_color,amount:_amount}));
+   totalSold++;
+   assert(totalSold > 0);
+ }
+
+ function MerchSold() public view returns (Merchandize[] memory){
+    return merchandize;
+ }
+}
